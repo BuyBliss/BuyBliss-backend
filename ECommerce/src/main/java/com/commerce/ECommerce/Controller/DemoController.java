ba0301d2df64fa52419e1c779072b06d04f2b913
Nihar1402 <ghorsenihar@gmail.com>
2025-01-02 23:06:12 +0530

    First Commit

--- ECommerce/src/main/java/com/commerce/ECommerce/Controller/DemoController.java
+++ ECommerce/src/main/java/com/commerce/ECommerce/Controller/DemoController.java
@@ -1,4 +1,12 @@
 package com.commerce.ECommerce.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class DemoController {
+    @GetMapping("/f5StatusCheck")
+    public void test()
+    {
+        System.out.println("Success");
+    }
 }
