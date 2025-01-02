cfdc26bf3fe73988a0289a14b4406bebda37d220
ShubhamMS7 <shubhamsuryawanshi2002@gmail.com>
2025-01-02 21:25:34 +0530

    Product class added

---
+++ ECommerce/src/main/java/com/commerce/ECommerce/Model/Product.java
@@ -0,0 +1,13 @@
+package com.commerce.ECommerce.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private String productName;
+    private String description;
+}
