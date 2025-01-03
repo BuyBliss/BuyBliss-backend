4429ae19b01a90b4197e65e8734810dce09070ba
ShubhamMS7 <shubhamsuryawanshi2002@gmail.com>
2025-01-03 18:53:12 +0530

    repository annotation added

--- ECommerce/src/main/java/com/commerce/ECommerce/Repositoy/ProductRepo.java
+++ ECommerce/src/main/java/com/commerce/ECommerce/Repositoy/ProductRepo.java
@@ -2,7 +2,9 @@ package com.commerce.ECommerce.Repositoy;
 
 import com.commerce.ECommerce.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
 }
